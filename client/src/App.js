37a74738d2c616f86de9b89c9893e1d1c0a600c1
TDMWalpala <tharindudarshana2019@gmail.com>
2023-05-30 09:30:39 +0530

    Added list items

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,17 @@
 import ListHeader from "./components/ListHeader";
-import { useEffect } from "react";
+import ListItem from "./components/ListItem";
+import { useEffect, useState } from "react";
 
 function App() {
+  const userEmail = 'tharindu@gmail.com';
+  const [tasks, setTasks] = useState(null)
   const getData = async () => {
-    const userEmail = 'tharindu@gmail.com';
+    
     try {
       const response = await fetch(`http://localhost:8000/todos/${userEmail}`);
       const json = await response.json();
-      console.log(json);
+      // console.log(json);
+      setTasks(json)
     } catch (error) {
       console.error(error);
     }
@@ -17,9 +21,15 @@ function App() {
     getData();
   }, []);
 
+  console.log(tasks);
+
+//sort tasks
+const sortedTasks = tasks?.sort((a,b)=> new Date(a.date) - new Date(b.date))
+
   return (
     <div className="app">
       <ListHeader listName={'🌲 Holiday tick list'}/>
+      {sortedTasks?.map(task => <ListItem key={task.id} task={task} />)}
     </div>
   );
 }
