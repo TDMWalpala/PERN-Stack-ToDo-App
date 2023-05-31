5fb25588df448c3c2af7b0aac5a3c772defa7b9b
TDMWalpala <tharindudarshana2019@gmail.com>
2023-05-31 16:26:56 +0530

    delete task completed

--- client/src/components/Listitem.js
+++ client/src/components/Listitem.js
@@ -7,6 +7,22 @@ import Modal from './Modal'
 const ListItem = ({ task , getData}) => {
   const [showModal, setShowModal] = useState(false)
 
+  const deleteItem = async() =>{
+    try {
+      const response = await fetch(`http://localhost:8000/todos/${task.id}`, {
+        method: 'DELETE',
+      });
+
+      if (response.status === 200) {
+        console.log("DELETE WORKED");
+        getData();
+      }
+  
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
   return (
     <li className='list-item'>
       <div className="info-container">
@@ -16,7 +32,7 @@ const ListItem = ({ task , getData}) => {
       </div>
       <div className="button-container">
         <button className='edit' onClick={()=> setShowModal(true)}>EDIT</button>
-        <button className='delete'>DELETE</button>
+        <button className='delete' onClick={deleteItem}>DELETE</button>
       </div>
       {showModal && <Modal mode={'edit'} setShowModal = {setShowModal} getData = {getData} task = {task} />}
     </li>
--- server/server.js
+++ server/server.js
@@ -53,5 +53,18 @@ app.put('/todos/:id', async (req, res) => {
   }
 });
 
+//delete a todo
+app.delete('/todos/:id', async (req, res) => {
+  const { id } = req.params
+  try {
+    const deleteToDo = await pool.query(
+      `DELETE FROM todos WHERE id = $1`,
+      [id]
+    );
+    res.json(deleteToDo);
+  } catch (error) {
+    console.error(error);
+  }
+});
 
 app.listen(PORT, () => console.log(`server running on PORT ${PORT}`));  // Starts the server and logs the port number
